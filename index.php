afcdcd16267161460c26a7a7a8c9aba2f6016429
andiniarlesta <arlestaandini861@gmail.com>
2025-07-22 15:12:45 +0700

    dashboard new

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="id">
 <head>
@@ -21,10 +20,21 @@
       <h1 class="text-2xl md:text-3xl font-bold text-white">SIMANTAP</h1>
     </div>
     <ul class="flex space-x-8 md:space-x-16 font-medium text-base md:text-lg">
-      <li><a href="index.php" class="hover:border-b-2 border-white">Beranda</a></li>
-      <li><a href="tentang.php" class="hover:border-b-2 border-white">Tentang Kami</a></li>
       <li>
-        <a href="contact.php" class="bg-[#006D77] hover:bg-[#005760] px-6 py-2 rounded-full text-white shadow-md transition">
+        <a href="index.php" class="relative group text-white">
+          Beranda
+          <span class="absolute left-0 bottom-[-4px] h-[2px] w-full bg-[#011527] scale-x-0 group-hover:scale-x-100 origin-left transition-transform duration-300"></span>
+        </a>
+      </li>
+      <li>
+        <a href="tentang.php" class="relative group text-white">
+          Tentang Kami
+          <span class="absolute left-0 bottom-[-4px] h-[2px] w-full bg-[#011527] scale-x-0 group-hover:scale-x-100 origin-left transition-transform duration-300"></span>
+        </a>
+      </li>
+      <li>
+        <a href="contact.php" class="bg-[#052748] hover:bg-[#011527] px-6 py-2 rounded-tl-[50px] rounded-tr-[50px]
+        rounded-br-[50px] rounded-bl-[0px]  text-white shadow-md transition">
           Contact
         </a>
       </li>
@@ -36,19 +46,20 @@
     <!-- Text Section -->
     <div class="max-w-xl space-y-6">
       <h2 class="text-5xl md:text-6xl text-gray-900 font-bold leading-snug font-times">
-        Sistem Informasi<br>Monitoring<br>Tugas Pegawai
+        Sistem Informasi Monitoring<br>Tugas Pegawai
       </h2>
       <p class="text-2xl text-gray-700">Aplikasi Monitoring Tugas</p>
-      <a href="page/login.php" class="inline-block bg-blue-800 hover:bg-blue-900 px-8 py-3 rounded text-white font-bold">
+      <a href="page/login.php" class="inline-block bg-[#052748] hover:bg-[#011527] px-10 py-3 rounded-[30px] text-white font-bold">
         LOGIN
       </a>
     </div>
 
     <!-- Image Section -->
     <div>
-      <img src="assets/bing.png" alt="Ilustrasi SIMANTAP" class="w-[600px] md:w-[600px] rounded-lg shadow-lg"/>
+      <img src="assets/bing.png" alt="Ilustrasi SIMANTAP" class="w-[600px] md:w-[600px] object-cover"/>
     </div>
   </div>
 
 </body>
 </html>
+
--- page/login.php
+++ page/login.php
@@ -9,49 +9,34 @@ include '../db.php';
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body class="bg-[#f5f5f5] min-h-screen flex items-center justify-center">
-
     <div class="bg-white shadow-lg rounded-lg flex max-w-4xl w-full overflow-hidden">
-        
         <div class="w-1/2 bg-white flex items-center justify-center p-10">
             <img src="../assets/4a903338c0e478248153bd8f3f6f6745.jpg" alt="Ilustrasi Login" class="max-w-full h-auto">
         </div>
-
-       
         <div class="w-1/2 p-10 flex flex-col justify-center">
-            <h2 class="text-2xl font-bold mb-8 text-black">LOGIN</h2>
+            <h2 class="text-2xl font-bold mb-8 text-black">
+                LOGIN
+            </h2>
             <form action="login_process.php" method="POST">
-
               <label for="nama" class="block text-sm text-gray-700 mb-1">Username:</label>
-<input type="text" id="nama" name="nama" required 
-    class="w-full px-4 py-2 rounded-md bg-[#dddddd] focus:outline-none focus:ring-2 focus:ring-blue-400">
-
-<label for="password" class="block text-sm text-gray-700 mb-1">Password:</label>
-<input type="password" id="password" name="password" required 
-    class="w-full px-4 py-2 rounded-md bg-[#dddddd] focus:outline-none focus:ring-2 focus:ring-blue-400">
-
-<label for="role" class="block text-sm text-gray-700 mb-1">Jabatan:</label>
-<select id="role" name="role" required
-    class="w-full px-4 py-2 rounded-md bg-[#dddddd] focus:outline-none focus:ring-2 focus:ring-blue-400">
-    <option value="admin">Admin</option>
-    <option value="penanggung jawab">Penanggung Jawab</option>
-    <option value="pengendali teknis">Pengendali Teknis</option>
-     <option value="ketua divisi">Ketua Divisi</option>
-    <option value="anggota">Anggota</option>
-</select>
-
-
-
-                    <button type="submit" 
+                <input type="text" id="nama" name="nama" required
+                    class="w-full px-4 py-2 rounded-md bg-[#dddddd] focus:outline-none focus:ring-2 focus:ring-blue-400">
+                <div class="space-y-4">
+                <div>
+                    <label for="password" class="block text-sm text-gray-700 mb-1">Password:</label>
+                    <input type="password" id="password" name="password" required
+                        class="w-full px-4 py-2 rounded-md bg-[#dddddd] focus:outline-none focus:ring-2 focus:ring-blue-400">
+                </div>
+                <button type="submit"
                         class="w-full bg-[#003366] text-white py-2 rounded-md font-semibold hover:bg-[#002244] transition duration-300">
-                        Login
-                    </button>
+                    Login
+                </button>
+                </div>
             </form>
-
             <p class="mt-4 text-sm text-center text-black">
                 Kembali Ke Beranda? <a href="../index.php" class="text-blue-600 hover:underline">Klik Disini</a>
             </p>
         </div>
     </div>
-
 </body>
 </html>
